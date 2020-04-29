ab03b3254a6466255901ecdfe1724def8a0462c5
Mher-777 <mherjan2003@mail.ru>
2020-04-30 00:31:32 +0400

    added discount section

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
 
 })
+const iconDown = (bottomScrollSection, scrollTopSection) => {
+	const bottomScroll = document.querySelector(bottomScrollSection),
+		elem = document.querySelector(scrollTopSection);
+	bottomScroll.addEventListener('click', (event) => {
+		const elemCountHeight = elem.scrollHeight
+		event.preventDefault()
+		window.scrollBy({top: elemCountHeight, behavior: "smooth"});
+	})
+}
+iconDown('.header__bottom-icon', '.s-discount')
