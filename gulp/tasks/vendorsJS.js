cabe7bfbfc282004964f2075451a5e839a44dbd2
Mher-777 <mherjan2003@mail.ru>
2020-04-30 21:03:54 +0400

    added prices section

--- gulp/tasks/vendorsJS.js
+++ gulp/tasks/vendorsJS.js
@@ -3,7 +3,9 @@ const concat = require('gulp-concat');
 
 const vendorsScripts = [
   'node_modules/svg4everybody/dist/svg4everybody.min.js',
-  'node_modules/jquery/dist/jquery.min.js'
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/mixitup/dist/mixitup.js',
+  'node_modules/aos/dist/aos.js'
 ];
 
 module.exports = function vendors(cb) {
