afb35c841c5199633123875060ffa7edad073c9a
Mher-777 <mherjan2003@mail.ru>
2020-04-30 16:09:09 +0400

    added basket section

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -11,3 +11,28 @@ const iconDown = (bottomScrollSection, scrollTopSection) => {
 	})
 }
 iconDown('.header__bottom-icon', '.s-discount')
+
+const select = () => {
+	let selectCurrent = document.querySelectorAll('.select__header'),
+		selectItem = document.querySelectorAll('.select__item');
+	selectCurrent.forEach((item) => {
+		item.addEventListener('click', (e) => {
+			e.currentTarget.parentElement.classList.toggle('is-open');
+		})
+	});
+	selectItem.forEach((item) => {
+		item.addEventListener('click', (e) => {
+			e.currentTarget.closest('.select').querySelector('.select__current').innerHTML = e.currentTarget.innerHTML;
+			e.currentTarget.closest('.select').classList.remove('is-open');
+		})
+	});
+};
+select()
+const sliceText = () => {
+	const text = document.querySelector('.basket__title'),
+			desc = document.querySelector('.basket__desc');
+	if(text.clientHeight > 350){
+		desc.classList.add('basket__desc--is-open')
+	}
+}
+sliceText()
